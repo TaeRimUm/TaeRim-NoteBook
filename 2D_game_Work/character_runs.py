3a1bef34192c322a554b180af8da83fc17f09f1f
TaeRimUm <112843584+TaeRimUm@users.noreply.github.com>
2022-09-26 15:26:00 +0900

    Update character_runs.py
    과제를 위한 달리기 실습

--- 2D_game_Work/character_runs.py
+++ 2D_game_Work/character_runs.py
@@ -6,6 +6,19 @@ character = load_image('run_animation.png')
 x = 0
 
 # 여기를 채우세요.
+frame = 0
 
+#for x in range(0, 800, 5): while 대신넣기
+while (x<800):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 0, 100,100, x, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    x += 5
+    delay(0.05)
+    get_events()
 
 close_canvas()
+
+#Sprite Sheet 검색하면 많~~이 나옴#
