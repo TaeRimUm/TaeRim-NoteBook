b88e710ad3b8ad65e9158273a18c1455cfc315d4
TaeRimUm <112843584+TaeRimUm@users.noreply.github.com>
2022-10-05 14:27:19 +0900

    Update HomeWork_Drill8.py
    이거 좀더 만져보자

--- 2D_game_Work/Lecture06_HandlingInputs/HomeWork_Drill8.py
+++ 2D_game_Work/Lecture06_HandlingInputs/HomeWork_Drill8.py
@@ -4,63 +4,79 @@ def handle_events():
     # fill here
     # 좌우 움직이기!!!!
     global running
-    global x, y  # 바깥쪽에 정의된 X이다!
+    global x #바깥쪽에 정의된 X이다!
+    global y #일단 추가
+    global xdir
+    global ydir
+
     events = get_events() #1. 입력 이벤트 폴링
     for event in events:
         if event.type == SDL_QUIT: #2. 이벤트 종류 구분
             running = False
         elif event.type == SDL_KEYDOWN:
 
-            # 아래 Tab키로 띄어쓰기 존내 중요함
+            # 아래 Tab키로 띄어쓰기 중요함
             if event.key == SDLK_RIGHT:  # 오른쪽 #3. 이벤트 실제 입력값 구하기
-                x = x + 10  # 증가
+                xdir += 1  # 증가
 
             elif event.key == SDLK_LEFT:  # 왼쪽
-                x = x - 10  # 감소
-                character.clip_draw(frame * 100, 100 * 1, 100, 100, x, -90)
-                update_canvas() #아니 캐릭터 사진에서 분명 위치 옮기는거 일텐데
+                xdir -= 1  # 감소
+                #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, -90)
+                #update_canvas()
 
-            if event.key == SDLK_UP:
-                y = y + 10 #아니 왜 안움직여
+            elif event.key == SDLK_UP:
+                ydir += 1
 
             elif event.key == SDLK_DOWN:
-                y = y - 10
+                ydir -= 1
 
+            
             elif event.key == SDLK_ESCAPE:
                 running = False
 
+
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                xdir -= 1
+            elif event.key == SDLK_LEFT:
+                xdir += 1
+
+            elif event.key == SDLK_UP:
+                ydir -= 1
+
+            elif event.key == SDLK_DOWN:
+                ydir += 1
+
+
+
+
 open_canvas()
 grass = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 running = True
-x = 800 // 2 #이게 뭔 값이였지# #800 // 2 전체 캔버스800에서 절반을 사용하겠다 는 의
-y = 800 // 2
-dir = 0
-pass
-#존내빠르게 호다다다다다닥하고 달림
+x = 800 // 2 #이게 뭔 값이였지# #800 // 2 전체 캔버스800에서 절반을 사용하겠다 는 의미
+xdir = 0
+ydir = 0
+y = 600 // 2 #얘는 내가 왜 넣었지.
 
-open_canvas()
-grass = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.png')
-
-running = True
-x = 800 // 2
-y = 800 // 2
-dir = 0
 frame = 0
 
 while running:
     clear_canvas()
     grass.draw(400, 200)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+
+    if xdir == 1:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+                                                       #x, y) 로 수정하면 대각선으로 움직임.
+                                                       #90, x)로 수정하면 좌, 우 키눌렀는데 위, 아래로 움직임.
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
-
+    x += xdir * 5
+    y += ydir * 5
     delay(0.01)
-
 
 
 close_canvas()
