e3f2127cb26f900033b562dfbed1244dadc8a9ce
TaeRimUm <112843584+TaeRimUm@users.noreply.github.com>
2022-10-11 15:53:30 +0900

    Create mygame.py

---
+++ 2D_game_Work/Lecture10_Game_Framework/mygame.py
@@ -0,0 +1,11 @@
+# fill here
+import pico2d
+import game_framework
+import logo_state
+
+pico2d.open_canvas()
+
+game_framework.run(logo_state)
+
+pico2d.close_canvas()
+#켜젔다 금방 꺼짐. logo_state가 잘 실행됨.
