f143dec04f58fe247f03b08a10090b6bc8685a3a
TaeRimUm <112843584+TaeRimUm@users.noreply.github.com>
2022-10-13 15:22:55 +0900

    Update play_state.py

--- 2D_game_Work/Lecture10_Game_Framework/play_state.py
+++ 2D_game_Work/Lecture10_Game_Framework/play_state.py
@@ -13,14 +13,25 @@ class Boy:
     def __init__(self):
         self.x, self.y = 0, 90
         self.frame = 0
-        self.image = load_image('run_animation.png')
+        self.dir = 1 # 오른쪽
+        self.image = load_image('animation_sheet.png')
 
     def update(self):
         self.frame = (self.frame + 1) % 8
-        self.x += 1
+        self.x += self.dir * 2
+        if self.x > 800:
+            self.x = 800
+            self.dir = -1 #왼쪽
+        elif self.x < 0:
+            self.x = 0
+            self.dir = 1
 
     def draw(self):
-        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+        if self.dir == 1:
+            self.image.clip_draw(self.frame*100, 100, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
 
 
 def handle_events():
@@ -28,25 +39,23 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            #running = False
             game_framework.quit()
-
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            #running = False
             game_framework.change_state(logo_state)
-            #esc 누르면 logo_state로 돌아가야 함.
 
 
-boy = None
+boy = None # c로 따지믄 NULL
 grass = None
 running = True
 
+# 초기화
 def enter():
     global boy, grass, running
     boy = Boy()
     grass = Grass()
     running = True
 
+# finalization code
 def exit():
     global boy, grass
     del boy
@@ -60,4 +69,8 @@ def draw():
     grass.draw()
     boy.draw()
     update_canvas()
+
+
+
+
 
