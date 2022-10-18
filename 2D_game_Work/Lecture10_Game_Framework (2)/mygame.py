07b73cc6ecf3b7800a149a4de90febaf3625748d
TaeRimUm <112843584+TaeRimUm@users.noreply.github.com>
2022-10-18 14:49:17 +0900

    Update mygame.py

--- 2D_game_Work/Lecture10_Game_Framework (2)/mygame.py
+++ 2D_game_Work/Lecture10_Game_Framework (2)/mygame.py
@@ -6,10 +6,13 @@ import play_state
 import item_state
 import add_delete_boy
 
+import play_state_Boy_move
+
 pico2d.open_canvas()
 #game_framework.run(logo_state)
-game_framework.run(play_state)
+#game_framework.run(play_state)
 #game_framework.run(item_state)
+game_framework.run(play_state_Boy_move)
 
 
 pico2d.close_canvas()
