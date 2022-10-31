352ec02ed787f5389269ef854325d50a05dec3e0
TaeRimUm <112843584+TaeRimUm@users.noreply.github.com>
2022-10-31 15:01:08 +0900

    Create dummy_test.py

---
+++ 2D_game_Work/Lecture11_Character_Controller/Lecture11_Character_Controller/dummy_test.py
@@ -0,0 +1,26 @@
+#>>> 이걸로 해도 됨. 아래 파이선 테이블
+
+>>> table = { "SLEEP": {"HIT":"WAKE"}, "WAKE": {"TIMER10":"SLEEP"} }
+>>> table["SLEEP"]["HIT"]
+'WAKE'
+
+>>> tavle['WAKE']['TIME10']
+''
+
+
+>>> q
+[]
+
+>>> q.insert(0, 'cat0')
+>>> q
+['cat0']
+>>> q = []
+>>> q.insert(0, 'dog1')
+>>> q
+['pig2', 'cat1']
+>>> q.insert(0, 'pig2')
+>>> q
+['']
+>>> q.pop()
+['cat1']
+
