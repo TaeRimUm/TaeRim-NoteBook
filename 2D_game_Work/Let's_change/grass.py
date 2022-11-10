c783fccaab6c339032e6dd174b7790f8d403319d
TaeRimUm <112843584+TaeRimUm@users.noreply.github.com>
2022-11-10 21:20:48 +0900

    Create grass.py

---
+++ 2D_game_Work/Let's_change/grass.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self): pass
+
+class Grass1:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 60)
+
+    def update(self): pass
+
+
